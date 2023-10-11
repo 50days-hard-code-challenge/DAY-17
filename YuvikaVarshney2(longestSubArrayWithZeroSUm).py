97e2d08cd5c484396a2fba362b69f8f9425c3061
YuvikaVarshney <130446237+YuvikaVarshney@users.noreply.github.com>
2023-10-11 20:53:01 +0530

    Create YuvikaVarshney2(longestSubArrayWithZeroSum).py

---
+++ YuvikaVarshney2(longestSubArrayWithZeroSUm).py
@@ -0,0 +1,40 @@
+def getLongestZeroSumSubarrayLength(arr : List[int]) -> int:
+    # Write your code here.
+    n = len(arr)
+
+    # maxLength is used to store the maximum
+    # length of a subarray whose sum = 'k'
+    maxLength = 0
+
+    # start and end are the starting
+    # and the ending indices of our current
+    # subarray
+    start = 0
+    end = -1
+    currentSum =0
+
+    # Iterating over 'a'
+    while start < n:
+
+        # Increasing the right end
+        # till our sum <= ‘k’ or we
+        # are not out of bounds
+        while (end + 1 < n):
+            currentSum += arr[end + 1]
+            end += 1
+            if currentSum == 0:
+                maxLength = max(maxLength, end - start + 1)
+
+
+        # We have found a subarray with the
+        #  required sum.
+        
+
+        # Shifting the start index
+        currentSum=0
+        start += 1
+        end=start-1
+
+    return maxLength
+
+    pass
